9f33af16cc380c35f9a23d8a362c231859134265
AxelAlejandroReséndizÁvila <124049803@upq.edu.mx>
2026-03-17 15:00:32 -0600

    Cambio de parametros en auth

--- SERVIRE DESKTOP/backend/src/middlewares/auth.js
+++ SERVIRE DESKTOP/backend/src/middlewares/auth.js
@@ -8,7 +8,7 @@ const authMiddleware = (req, res, next) => {
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET || 'servire_secret_key');
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.usuario = decoded.id;
         next();
     } catch (err) {
